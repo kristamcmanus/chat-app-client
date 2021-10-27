8811ccbd94cc213a524b8ac3f127a03299064b1c
kristamcmanus <k.mcmanus310@gmail.com>
2021-10-26 21:18:52 -0400

    Add necessary folders and files and stream api key

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey = 'bcfhsfsy3vdh';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Dev Chat App</h1>
+    </div>
+  );
+}
+
+export default App;
