747187af8a5f125c944ef501de1d15ed4cdd4709
kristamcmanus <k.mcmanus310@gmail.com>
2021-10-30 17:43:10 -0400

    Add styling and additional component files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
+import { ChannelContainer, ChannelListContainer } from './components';
+import './App.css';
 
+// initialize the chat
 const apiKey = 'bcfhsfsy3vdh';
+
+// create instance of stream chat
+// call as function and pass in apiKey
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Dev Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+
+        />
+        <ChannelContainer
+
+        />
+      </Chat>
     </div>
   );
 }
